2a4ada75665bbe91a869c10ff57b7b751e4b0b54
yinyong02 <yinyong02@baidu.com>
2015-12-29 18:43:27 +0800

    saved

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ const vo = require('vo');
 
 function* work(url, times, sniff) {
     const n = Nightmare({
-        width: 2560,
-        height: 1440,
-        show: false
+        width: 750,
+        height: 1334,
+        show: true
     });
 
     var gPerformance = [];
 
     var loaded = false;
 
-    // iPhone 6
     n.useragent(
+        //'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
         'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4'
     );
 
@@ -37,7 +37,7 @@ function* work(url, times, sniff) {
 
     while (totalTimes) {
         if (times === totalTimes) {
-            yield n.goto(url);
+            yield n.goto(url).cookies.set([{"name":"IS_NEW_USER","value":"ff4105375b059388a1baa019"},{"name":"TIEBA_USERTYPE","value":"280cd18fbff0e1459fef99e9"},{"name":"BAIDUID","value":"C94905664F21F36045F89D81FC72E2F2:FG"},{"name":"PSTM","value":"1436347344"},{"name":"BIDUPSID","value":"239CF6D14826C64E593283EDBCC283FA"},{"name":"TIEBAUID","value":"dbdb3be1dea9006fce181a46"},{"name":"MCITY","value":"-%3A"},{"name":"CLIENTWIDTH","value":"375"},{"name":"CLIENTHEIGHT","value":"627"},{"name":"bdshare_firstime","value":"1448937926526"},{"name":"SEENKW","value":"%E6%88%98%E8%88%B0%E4%B8%96%E7%95%8C%23%E9%AD%94%E5%85%BD%E4%B8%96%E7%95%8C"},{"name":"SET_PB_IMAGE_WIDTH","value":"355"},{"name":"plus_lsv","value":"c17e48b2a2f01886"},{"name":"PLUS","value":"1"},{"name":"BDSFRCVID","value":"kMksJeC62mwsFRc4esy5hFElWg5G1PvTH6aoo89JCfZ2_7AYCnJtEG0PtvlQpYD-0kYVogKK0mOTHUvP"},{"name":"H_BDCLCKID_SF","value":"tb4f_ILXtDvbfP0kb4QEKPteqxby26n3JnneaJ5nJDonKfJmjtIMbJtT3x7l047U52393fOvQpP-HJ7zQhjJLnK0Qh3vBMoM5IJvKl0MLpF5DMjxWf8VQ-tO2xnMBMPjamOnaU5KLIFahKvmq4QJK4_H5hb-2C62aKDsBJky-hcqEIL4jxrh0UCgjMoeQxc33eTQ-b3cLfPBHUbSj4QoKJcXyRQwBbJI-6nMaPcwBq5nhMJmjPvGKhFvqto7-P6y523ion6vQpnlfxtujjLhej53eaRtaRO-26btWjra-IbJKROvhjROXtPyyxomtjjLb57lbJ8254OBMJnDQtvRbTL1DJ3qLUkqKCOyVITF0bQAhxA6LqrThxI3QttjQn3qaN6jhl-EtnvZSR7TyU42hf47yboqQTT2-DA_VU5"},{"name":"BCLID","value":"8800641420590885597"},{"name":"H_PS_PSSID","value":"17745_1454_17924_18156_17944_17001_17072_15066_12198_16096_18019"},{"name":"wise_device","value":"0"},{"name":"baidu_broswer_setup_殷勇02","value":"0"},{"name":"LONGID","value":"1505449736"}]);
         } else {
             yield n.refresh();
         }
--- test.js
+++ test.js
@@ -11,21 +11,60 @@
  */
 const np = require('./');
 
-const TRY_TIMES = 50;
+const TRY_TIMES = 1;
 
-const AD = 'http://fes1.tieba.baidu.com/mo/q/m?word=%E9%AD%94%E5%85%BD%E4%B8%96%E7%95%8C&page_from_search=index&tn6=bdISP&tn4=bdKSW&tn7=bdPSB&lm=16842752&lp=6093&sub4=%E8%BF%9B%E5%90%A7&pn=0';
-const NOAD = 'http://fes1.tieba.baidu.com/mo/q/m?word=%E9%AD%94%E5%85%BD%E4%B8%96%E7%95%8C&page_from_search=index&tn6=bdISP&tn4=bdKSW&tn7=bdPSB&lm=16842752&lp=6093&sub4=%E8%BF%9B%E5%90%A7&pn=0&noad=1';
+var URLS = ["http://tieba.baidu.com/p/4230999316?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4228627576?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4212166985?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4207174805?lp=5028&mo_device=1&new_word=dota2&is_jingpost=0", "http://tieba.baidu.com/p/4231298325?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231404033?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4230887697?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231380905?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231116854?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231381703?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4230483894?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231387990?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231366247?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4229766678?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4227241661?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231237678?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231373871?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4229510094?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231395881?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4229587624?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231395047?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4229191640?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4230706131?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4229912561?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231399586?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4230520813?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231332407?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231389880?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4229156946?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4229616492?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231288091?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231128651?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4228265367?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231396238?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/3654677220?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4230883795?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4230931398?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231352047?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231102146?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4230348784?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231080236?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4220952912?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231340831?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4230952449?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4229662582?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231400813?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4230953200?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231237131?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4228980720?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231384578?lp=5028&mo_device=1&is_jingpost=0", "http://tieba.baidu.com/p/4231051919?lp=5028&mo_device=1&is_jingpost=0"];
+
+const AD = URLS.slice();
+const NOAD = URLS.map(function (item) {
+    return item += '&lazy=1';
+});
+
+var AD_RESULTS = [];
+var NOAD_RESULTS = [];
+
+function run() {
+    var top = AD.shift();
+    if (top) {
+        np(top, TRY_TIMES).then((performance) => {
+            AD_RESULTS = performance.concat(AD_RESULTS);
+            run();
+        }, (err) => {
+            console.error(err.message);
+        });
+    } else {
+        require('fs').writeFileSync('result-ad.json', JSON.stringify(AD_RESULTS, null, 2));        
+    }
+}
+
+function runLazy() {
+    var top = NOAD.shift();
+    if (top) {
+        np(top, TRY_TIMES).then((performance) => {
+            NOAD_RESULTS = performance.concat(NOAD_RESULTS);
+            runLazy();
+        }, (err) => {
+            console.error(err.message);
+        });
+    } else {
+        require('fs').writeFileSync('result-noad.json', JSON.stringify(NOAD_RESULTS, null, 2));        
+    }
+}
 
 //with ad
-np(AD, TRY_TIMES).then((performance) => {
+/*np(AD, TRY_TIMES).then((performance) => {
     require('fs').writeFileSync('result-ad.json', JSON.stringify(performance, null, 2));
 }, (err) => {
     console.error(err.message);
-});
+});*/
 
 //no ad
-np(NOAD, TRY_TIMES).then((performance) => {
+/*np(NOAD, TRY_TIMES).then((performance) => {
     require('fs').writeFileSync('result-noad.json', JSON.stringify(performance, null, 2));
 }, (err) => {
     console.error(err.message);
-});
+});*/
+
+run();
+
+runLazy();
